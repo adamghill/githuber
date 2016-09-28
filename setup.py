9c9ec48f8a6266e9a5e0a4844ea3f47c97eb6a3f
Adam Hill <adamghill@yahoo.com>
2016-09-28 14:11:37 -0400

    Update `click` from 6.2 to 6.6.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     py_modules=['githuber'],
     install_requires=[
-        'click==6.2',
+        'click==6.6',
         'functools32==3.2.3.post2',
         'github3.py==1.0.0a2',
         'grin==1.2.1',
